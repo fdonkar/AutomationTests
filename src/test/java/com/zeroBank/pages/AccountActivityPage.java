7e4eec1b92f5c894232347b51f78566e1e6ebee2
fdonkar <fdonkar@gmail.com>
2022-02-02 12:11:22 +0300

    Default select option test added

---
+++ src/test/java/com/zeroBank/pages/AccountActivityPage.java
@@ -0,0 +1,12 @@
+package com.zeroBank.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountActivityPage extends BasePage{
+
+    @FindBy(css ="#aa_accountId")
+    public WebElement accountDropdown;
+
+
+}
--- src/test/java/com/zeroBank/tests/AccountActivityTest.java
+++ src/test/java/com/zeroBank/tests/AccountActivityTest.java
@@ -1,6 +1,8 @@
 package com.zeroBank.tests;
 
+import com.zeroBank.pages.AccountActivityPage;
 import com.zeroBank.pages.LoginPage;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,5 +14,16 @@ public class AccountActivityTest extends TestBase{
         loginPage.login();
         loginPage.accountActivity.click();
         Assert.assertEquals(driver.getTitle(),"Zero - Account Activity", "Title NOT matched");
+    }
+
+    @Test
+    public void accountDropdownTest(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        loginPage.accountActivity.click();
+        AccountActivityPage accountActivityPage = new AccountActivityPage();
+        Select accountSelect = new Select(accountActivityPage.accountDropdown);
+        Assert.assertEquals(accountSelect.getFirstSelectedOption().getText(),"Savings",
+                "Default option NOT Savings");
     }
 }
